69d3fff3bd426c38d5492fd8b1ad5cdb8a460d40
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-07-17 12:00:20 +0200

    primitive FOXML generation

--- src/main/scala/nl/knaw/dans/easy/stage/Main.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/Main.scala
@@ -1,6 +1,6 @@
 package nl.knaw.dans.easy.stage
 
-import java.io.File
+import java.io.{PrintWriter, File}
 import java.nio.file.Paths
 
 import scala.util.Try
@@ -35,12 +35,46 @@ object Main {
     val sdoDir = getDODir(file)
     println("file DO> " + sdoDir.getPath)
     sdoDir.mkdir()
+
+    createFileFOXML(sdoDir)
   }
 
   def createDirDO(dir: File)(implicit s: Settings): Try[Unit] = Try {
     val sdoDir = getDODir(dir)
     println("dir DO> " + sdoDir.getPath)
     sdoDir.mkdir()
+  }
+
+  def createFileFOXML(sdoDir: File): Try[Unit] = Try {
+    val pw = new PrintWriter(Paths.get(sdoDir.getPath, "fo.xml").toFile)
+    pw.write(getFileFOXML())
+    pw.close()
+  }
+
+  def getFileFOXML(): String = {
+//    <?xml version="1.0" encoding="UTF-8"?>
+    <foxml:digitalObject VERSION="1.1"
+                         xmlns:foxml="info:fedora/fedora-system:def/foxml#"
+                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                         xsi:schemaLocation="info:fedora/fedora-system:def/foxml# http://www.fedora.info/definitions/1/0/foxml1-1.xsd">
+      <foxml:objectProperties>
+        <foxml:property NAME="info:fedora/fedora-system:def/model#state" VALUE="Active"/>
+      </foxml:objectProperties>
+      <foxml:datastream ID="DC" STATE="A" CONTROL_GROUP="X" VERSIONABLE="true">
+        <foxml:datastreamVersion ID="DC1.0" LABEL="Dublin Core Record for this object"
+                                 MIMETYPE="text/xml" FORMAT_URI="http://www.openarchives.org/OAI/2.0/oai_dc/">
+          <foxml:xmlContent>
+            <oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+                       xmlns:dc="http://purl.org/dc/elements/1.1/"
+                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                       xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+              <dc:identifier>ingest-test:1</dc:identifier>
+              <dc:description>An object ingested by posting a FOXML document to the REST API.</dc:description>
+            </oai_dc:dc>
+          </foxml:xmlContent>
+        </foxml:datastreamVersion>
+      </foxml:datastream>
+    </foxml:digitalObject>.mkString
   }
 
   def getDODir(fileOrDir: File)(implicit s: Settings): File = {
