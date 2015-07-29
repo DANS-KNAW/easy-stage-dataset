d93da9b88d6100a53a81c96c5a536dd83a60da9f
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-07-29 16:59:17 +0200

    added JSON config setup for datasets

--- src/main/scala/nl/knaw/dans/easy/stage/JSON.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/JSON.scala
@@ -11,14 +11,29 @@ import scala.util.Try
 
 object JSON {
 
+  def createDatasetCfg(sdoDir: File): Try[Unit] = {
+    val sdoCfg =
+      ("namespace" -> "easy-dataset") ~
+      ("datastreams" -> List()) ~
+      ("relations" -> List(
+        ("predicate" -> "http://dans.knaw.nl/ontologies/relations#hasDoi") ~ ("object" -> "10.5072/dans-zzz-zzz5"), // TODO: remove hard-coded DOI
+        ("predicate" -> "http://dans.knaw.nl/ontologies/relations#hasPid") ~ ("object" -> "urn:nbn:nl:ui:13-0000-00"), // TODO: remove hard-coded URN PID
+        ("predicate" -> "http://dans.knaw.nl/ontologies/relations#:isMemberOf") ~ ("objectSDO" -> "info:fedora/easy-discipline:1"), // TODO: remove hard-coded discipline
+        ("predicate" -> "info:fedora/fedora-system:def/model#hasModel") ~ ("object" -> "info:fedora/dans-model:recursive-item-v1"),
+        ("predicate" -> "info:fedora/fedora-system:def/model#hasModel") ~ ("object" -> "info:fedora/easy-model:EDM1DATASET"),
+        ("predicate" -> "info:fedora/fedora-system:def/model#hasModel") ~ ("object" -> "info:fedora/easy-model:oai-item1")
+      ))
+    writeToFile(new File(sdoDir.getPath, JSON_CFG_FILENAME), pretty(render(sdoCfg)))
+  }
+
   def createFileCfg(fileLocation: String, mimeType: String, parentSDO: String, sdoDir: File): Try[Unit] = {
     val sdoCfg =
       ("namespace" -> "easy-file") ~
       ("datastreams" -> List(
         ("dsLocation" -> fileLocation ) ~
-          ("dsID" -> "EASY_FILE") ~
-          ("controlGroup" -> "R") ~
-          ("mimeType" -> mimeType))) ~
+        ("dsID" -> "EASY_FILE") ~
+        ("controlGroup" -> "R") ~
+        ("mimeType" -> mimeType))) ~
       ("relations" -> List(
         ("predicate" -> "http://dans.knaw.nl/ontologies/relations#:isMemberOf") ~ ("objectSDO" -> parentSDO),
         ("predicate" -> "http://dans.knaw.nl/ontologies/relations#:isSubordinateTo") ~ ("objectSDO" -> DATASET_SDO),
