a9f52750014a69d2ea756454720013358fa7a8bd
Jo Pol <joke.pol@dans.knaw.nl>
2015-10-30 10:42:47 +0100

    connect just once to fedora, load disciplines just once

--- src/main/scala/nl/knaw/dans/easy/stage/Fedora.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/Fedora.scala
@@ -1,20 +1,37 @@
 package nl.knaw.dans.easy.stage
 
+import java.io.File
+
+import com.yourmediashelf.fedora.client.request.FedoraRequest
 import com.yourmediashelf.fedora.client.{FedoraClient, FedoraCredentials}
+import org.apache.commons.configuration.PropertiesConfiguration
 
 import scala.collection.JavaConversions._
 import scala.xml.XML
 
 object Fedora {
 
-  def loadDisciplines(fedoraServiceUrl: String, fedoraUser: String, fedoraPassword: String): Map[String,String] = {
+  def connect(fedoraServiceUrl: String, fedoraUser: String, fedoraPassword: String): Unit = {
     val credentials = new FedoraCredentials(fedoraServiceUrl, fedoraUser, fedoraPassword)
-    val client = new FedoraClient(credentials)
-    FedoraClient.findObjects().query("pid~easy-discipline:*").maxResults(Integer.MAX_VALUE).pid().execute(client).getPids
-      .map(pid => (FedoraClient.getRelationships(pid).execute(client).getEntity(classOf[String]), pid))
-      .map{ case (xml, pid) => ((XML.loadString(xml) \\ "Description" \ "setSpec").text, pid) }
-      .map{ case (disciplines, pid) => (disciplines.split(':').toSeq.last, pid) }
-      .toMap
+    FedoraRequest.setDefaultClient(new FedoraClient(credentials))
   }
 
+  def connect(): Unit = {
+    val file = new File(System.getProperty("app.home"), "cfg/application.properties")
+    val props = new PropertiesConfiguration(file)
+    Fedora.connect(
+      props.getString("fcrepo-service-url"),
+      props.getString("fcrepo-user"),
+      props.getString("fcrepo-password")
+    )
+  }
+
+  lazy val disciplines: Map[String,String] = {
+    FedoraClient.findObjects().query("pid~easy-discipline:*")
+      .maxResults(Integer.MAX_VALUE).pid().execute().getPids
+      .map(pid => (FedoraClient.getRelationships(pid).execute().getEntity(classOf[String]), pid))
+      .map{ case (xml, pid) => ((XML.loadString(xml) \\ "Description" \ "setSpec").text, pid) }
+      .map{ case (s, pid) => (s.split(':').toSeq.last, pid) }
+      .toMap
+  }
 }
