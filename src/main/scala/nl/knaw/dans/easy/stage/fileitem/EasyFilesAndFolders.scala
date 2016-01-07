baae9964584c3bb09eee27152c4ffd8a4a79a18b
Jo Pol <joke.pol@dans.knaw.nl>
2016-01-07 18:23:50 +0100

    unable to find folders because of trailing slashes

--- src/main/scala/nl/knaw/dans/easy/stage/fileitem/EasyFilesAndFolders.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/fileitem/EasyFilesAndFolders.scala
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2015-2016 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package nl.knaw.dans.easy.stage.fileitem
 
 import java.io.File
@@ -32,7 +17,7 @@ object EasyFilesAndFolders {
 
   def getPathId(path: File, datasetSid: String): Try[Option[String]] = Try {
     val query: PreparedStatement = conn.prepareStatement("SELECT pid FROM easy_folders WHERE dataset_sid = ? and path = ?")
-    query.setString(2, path.toString +"/")
+    query.setString(2, path.toString)
     query.setString(1, datasetSid)
     log.debug(s"$query")
     query.closeOnCompletion()
@@ -52,12 +37,14 @@ object EasyFilesAndFolders {
       .split("/")
       .scanLeft("")((acc, next) => acc + next + "/")
       .reverse
+      .map(_.replaceAll("/$",""))
       .find(path => {
-          query.setString(1, path)
-          val resultSet = query.executeQuery()
-          if(!resultSet.next()) throw new RuntimeException("Count query returned no rows (?) A count query should ALWAYS return one row")
-          resultSet.getString("count") == "1"
-        })
+        query.setString(1, path)
+        val resultSet = query.executeQuery()
+        // TODO don't forget to close the query?
+        if(!resultSet.next()) throw new RuntimeException("Count query returned no rows (?) A count query should ALWAYS return one row")
+        resultSet.getString("count") == "1"
+      })
     query.close()
     parentPath
   }
