5a522bb316c2e2f03fff22d82c2f27db1a0b99ac
Jo Pol <joke.pol@dans.knaw.nl>
2016-01-08 10:42:41 +0100

    restored license header lost whith rebasing

--- src/main/scala/nl/knaw/dans/easy/stage/fileitem/EasyFilesAndFolders.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/fileitem/EasyFilesAndFolders.scala
@@ -1,14 +1,28 @@
+/**
+ * Copyright (C) 2015-2016 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package nl.knaw.dans.easy.stage.fileitem
 
 import java.io.File
-import java.sql.{PreparedStatement, DriverManager}
+import java.sql.{DriverManager, PreparedStatement}
 
-import nl.knaw.dans.easy.stage.fileitem.EasyStageFileItem._
-import nl.knaw.dans.easy.stage.lib.Props
 import nl.knaw.dans.easy.stage.lib.Props.props
 import org.slf4j.LoggerFactory
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 object EasyFilesAndFolders {
   val log = LoggerFactory.getLogger(getClass)
