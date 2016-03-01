57ebfe9a4c21b893e9ce7ca0410cb7d840d9a6e0
Joke Pol <joke.pol@dans.knaw.nl>
2016-03-01 13:01:58 +0100

    provided shorts and implemented validation for the options added for EASY-978 by #65

--- src/main/scala/nl/knaw/dans/easy/stage/fileitem/FileItemSettings.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/fileitem/FileItemSettings.scala
@@ -18,29 +18,44 @@ package nl.knaw.dans.easy.stage.fileitem
 import java.io.File
 import java.net.URL
 
+import nl.knaw.dans.easy.stage.fileitem.FileItemSettings._
 import nl.knaw.dans.easy.stage.lib.Fedora
-import nl.knaw.dans.easy.stage.lib.Props._
+import nl.knaw.dans.easy.stage.lib.Props.props
+
+import scala.util.Try
 
 case class FileItemSettings (sdoSetDir: Option[File],
-                                     datasetId: Option[String],
-                                     datastreamLocation: Option[URL],
-                                     unsetUrl: URL = new URL(props.getString("redirect-unset-url")),
-                                     size: Option[Long],
-                                     ownerId: String = props.getString("owner"),
-                                     pathInDataset: Option[File],
-                                     title:  Option[String] = None,
-                                     format: Option[String] = None,
+                             datasetId: Option[String],
+                             datastreamLocation: Option[URL],
+                             unsetUrl: URL = new URL(props.getString("redirect-unset-url")),
+                             size: Option[Long],
+                             ownerId: String = props.getString("owner"),
+                             pathInDataset: Option[File],
+                             title:  Option[String] = None,
+                             format: Option[String] = None,
 
-                                     // as in SDO/*/EASY_FILE_METADATA
-                                     creatorRole: String = "DEPOSITOR",
-                                     visibleTo: String = "ANONYMOUS",
-                                     accessibleTo: String = "NONE",
-                                     fedora: Fedora = Fedora,
-                                     easyFilesAndFolders: EasyFilesAndFolders = EasyFilesAndFolders,
+                             // as in SDO/*/EASY_FILE_METADATA
+                             creatorRole: String = defaultCreatorRole,
+                             visibleTo: String = defaultVisibleTo,
+                             accessibleTo: String = defaultAccessibleTo,
+                             fedora: Fedora = Fedora,
+                             easyFilesAndFolders: EasyFilesAndFolders = EasyFilesAndFolders,
 
-                                     subordinate: (String, String))
+                             subordinate: (String, String)) {
+  if (!FileItemSettings.creatorRoles.contains(creatorRole))
+    throw new Exception(s"illegal value for creatorRole, got $creatorRole")
+  if (!FileItemSettings.accessCategories.contains(visibleTo))
+    throw new Exception(s"illegal value for visibleTo, got $visibleTo")
+  if (!FileItemSettings.accessCategories.contains(accessibleTo))
+    throw new Exception(s"illegal value for accessibleTo, got $accessibleTo")
+}
 
 object FileItemSettings {
+  val defaultCreatorRole = "DEPOSITOR"
+  val defaultVisibleTo = "ANONYMOUS"
+  val defaultAccessibleTo = "NONE"
+  val accessCategories =  Array("ANONYMOUS", "KNOWN", "RESTRICTED_REQUEST", "RESTRICTED_GROUP", "NONE")
+  val creatorRoles =  Array("ARCHIVIST", "DEPOSITOR")
 
   /** new file or folder for a new dataset */
   def apply(sdoSetDir: File,
@@ -50,6 +65,7 @@ object FileItemSettings {
             title: Option[String],
             size: Option[Long]
            ) =
+    // no need to catch exceptions thrown by the constructor as the defaults take care of valid values
     new FileItemSettings(
       sdoSetDir = Some(sdoSetDir),
       datasetId = None,
@@ -64,17 +80,18 @@ object FileItemSettings {
 
   /** new file or folder for an existing dataset */
   def apply(conf: FileItemConf) =
+    // no need to catch exceptions thrown by the constructor as FileItemConf performs the same checks
     new FileItemSettings(
       sdoSetDir = conf.sdoSetDir.get,
       datastreamLocation = conf.dsLocation.get,
       size = conf.size.get,
-      accessibleTo = conf.accessibleTo.get.map(_.trim).filter(_.nonEmpty).getOrElse("ANONYMOUS"),
-      visibleTo = conf.visibleTo.get.map(_.trim).filter(_.nonEmpty).getOrElse("NONE"),
-      creatorRole = conf.creatorRole.get.map(_.trim).filter(_.nonEmpty).getOrElse("DEPOSITOR"),
+      accessibleTo = conf.accessibleTo(),
+      visibleTo = conf.visibleTo(),
+      creatorRole = conf.creatorRole(),
       ownerId = conf.ownerId.get.map(_.trim).filter(_.nonEmpty).getOrElse(props.getString("owner")),
       datasetId = conf.datasetId.get,
       pathInDataset = conf.pathInDataset.get,
-      format = conf.format.orElse( Some("application/octet-stream")).get,
+      format = conf.format.get,
       subordinate = "object" -> s"info:fedora/${conf.datasetId()}"
     ) {
       override def toString = conf.toString
