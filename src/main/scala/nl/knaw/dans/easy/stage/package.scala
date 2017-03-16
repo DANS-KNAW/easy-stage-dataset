84416cd9c33b017b8fede2d8302d3fd63d817a84
Paul Boon <paul.boon@dans.knaw.nl>
2017-03-16 09:31:45 +0100

    TryLogic now works with &&& and |||

--- src/main/scala/nl/knaw/dans/easy/stage/package.scala
+++ src/main/scala/nl/knaw/dans/easy/stage/package.scala
@@ -20,6 +20,7 @@ import java.net.{HttpURLConnection, URL}
 import nl.knaw.dans.pf.language.ddm.handlermaps.NameSpace
 
 import scala.util.{Failure, Success, Try}
+import scala.language.implicitConversions
 
 package object stage {
 
@@ -45,24 +46,57 @@ package object stage {
   /*
    Logic opperators that work with Try[Boolean]
    TODO future candidate for dans-scala-lib.
+   TODO investigate why it doesn't (maven) build with the standard && and || for the case 'Boolean && Try[Boolean]'
+     error: type mismatch;
+      found   : scala.util.Try[Boolean]
+      required: Boolean
+            s.stageFileDataAsRedirectDatastreams || (s.stubAVfiles && isAVType(bagRelativePath))
    */
   implicit class TryLogic(val t: Try[Boolean]) extends AnyVal {
-    def &&(t2: => Try[Boolean]): Try[Boolean] = {
+    def &&&(t2: => Try[Boolean]): Try[Boolean] = {
       t.flatMap {
         case true => t2
         case _ => Success(false)
       }
     }
 
-    def ||(t2: => Try[Boolean]): Try[Boolean] = {
+    // Using DummyImplicit to prevent 'have same type after type erasure' !
+    def &&&(b: => Boolean)(implicit d: DummyImplicit): Try[Boolean] = {
+      t.map {
+        case true => b
+        case _ => false
+      }
+    }
+
+    def |||(t2: => Try[Boolean]): Try[Boolean] = {
       t.flatMap {
         case false => t2
         case _ => Success(true)
       }
     }
 
+    // Using DummyImplicit to prevent 'have same type after type erasure' !
+    def |||(b: => Boolean)(implicit d: DummyImplicit): Try[Boolean] = {
+      t.map {
+        case false => b
+        case _ => true
+      }
+    }
+
     def unary_! : Try[Boolean] = {
       t.map(!_)
+    }
+  }
+
+  implicit class BooleanLogicWithTry(val b: Boolean) extends AnyVal {
+    def &&&(t2: => Try[Boolean]): Try[Boolean] = {
+      if (b) t2
+      else Success(false)
+    }
+
+    def |||(t2: => Try[Boolean]): Try[Boolean] = {
+      if (b) Success(true)
+      else t2
     }
   }
 
